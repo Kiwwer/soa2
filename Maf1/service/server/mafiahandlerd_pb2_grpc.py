8f6f4a5570bc7d02d5f9bc2554416b358fe4bfc4
Kiwwer <71013207+Kiwwer@users.noreply.github.com>
2023-06-26 22:07:25 +0300

    Fix connectivity

--- Maf1/service/server/mafiahandlerd_pb2_grpc.py
+++ Maf1/service/server/mafiahandlerd_pb2_grpc.py
@@ -2,7 +2,7 @@
 """Client and server classes corresponding to protobuf-defined services."""
 import grpc
 
-import mafiahandlerd_pb2 as mafiahandler__pb2
+import service.server.mafiahandlerd_pb2 as mafiahandler__pb2
 # service.server.
 
 class EngineServerStub(object):
